c7f4236e3fac1cac2a46b0c5b1475ed7ea39c263
KatotenMk2 <cino1031@gmail.com>
2022-07-07 13:46:48 +0900

    banner image fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './styles/App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = '_UNCHAIN';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
