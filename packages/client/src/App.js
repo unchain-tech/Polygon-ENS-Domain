9620679c8bbdea840207438bfbd23cf22583ab8e
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-10-11 18:07:41 +0900

    refactor: change indent from tabs to spaces

--- packages/client/src/App.js
+++ packages/client/src/App.js
@@ -9,30 +9,30 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const App = () => {
 
   return (
-		<div className="App">
-			<div className="container">
+    <div className="App">
+      <div className="container">
 
-				<div className="header-container">
-					<header>
+        <div className="header-container">
+          <header>
             <div className="left">
               <p className="title">🐱‍👤 Ninja Name Service</p>
               <p className="subtitle">Your immortal API on the blockchain!</p>
             </div>
-					</header>
-				</div>
+          </header>
+        </div>
 
         <div className="footer-container">
-					<img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
-					<a
-						className="footer-text"
-						href={TWITTER_LINK}
-						target="_blank"
-						rel="noreferrer"
-					>{`built with @${TWITTER_HANDLE}`}</a>
-				</div>
-			</div>
-		</div>
-	);
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built with @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
